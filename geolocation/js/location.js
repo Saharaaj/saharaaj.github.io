57fcbf15751efc8929e319dcc2413cbb4ae3533c
saharaaj <raajsaha052@gmail.com>
2021-10-01 21:53:10 +0800

    geo

--- geolocation/js/location.js
+++ geolocation/js/location.js
@@ -1,15 +1,15 @@
 let elLocate = document.getElementById("btnLocate")
-elLocate.addEventListener("click",function(position){
+elLocate.addEventListener("click",function(){
 
     if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(function(position){
-        let elLat = document.getElementById("lat")
-        let elLong = document.getElementById("long")
-        elLat.innerHTML = "Latitude: " + position.coords.latitude
-        elLong.innerHTML = "Longitude: " + position.coords.longtude
-    })
-}else {
-    alert("Geolocation is not supported!")
-}
+        navigator.geolocation.getCurrentPosition(function(position){
+            let elLat = document.getElementById("lat")
+            let elLong = document.getElementById("long")
+            elLat.innerHTML = "Latitude: " + position.coords.latitude
+            elLong.innerHTML = "Longitude: " + position.coords.longtude
+        })
+    } else {
+        alert("Geolocation is not supported!")
+    }
     
-})
+   })
