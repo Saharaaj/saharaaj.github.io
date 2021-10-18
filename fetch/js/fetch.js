095056af8ea1e18ef5eaaee536c3473df9d4ef99
saharaaj <raajsaha052@gmail.com>
2021-10-18 14:53:24 +0800

    added fetch

--- fetch/js/fetch.js
+++ fetch/js/fetch.js
@@ -20,3 +20,71 @@ let elLoadUser = document.getElementById("getRandomUser")
 elLoadUser.addEventListener("click", function(){
     LoadUser()
 })
+
+let elLoadMultipleUsers = document.getElementById("getMultipleUser")
+elLoadMultipleUsers.addEventListener("click",function(){
+    let userCount = document.getElementById("userCount")
+    LoadMultipleUsers(userCount.value)
+})
+
+function LoadMultipleUsers(userCount){
+    let url = "https://randomuser.me/api/?results=" + userCount
+    let temp = ""
+
+    fetch(url)
+    .then((response) => response.json())
+    .then(data => {
+        let allUsers = document.getElementById("allUsers")
+
+        for(let i = 0; i < data.results.length; i++){
+            let fn = '<div>'+ data.results[i].name.first + '</div>'
+            let ln = '<div>'+ data.results[i].name.first + '</div>'
+            let phone = '<div>'+ data.results[i].phone + '</div>'
+            let img = '<img src="' + data.results[i].picture.large + '">'
+
+            temp = temp + fn + ln + phone + img
+        }
+        allUsers.innerHTML = temp 
+    })
+}
+
+let elLoadGender = document.getElementById("getGenderUser")
+elLoadGender.addEventListener("click",function(){
+    console.log("Load Gender clicked!")
+    //get the gender
+    let gender = document.getElementById("userGender")
+    console.log("Selected Gender: " + gender.value)
+
+    //get the number of result
+    let count = document.getElementById("userGenderCount")
+    console.log("Count: " + count.value)
+
+    //call loadGender() and pass gender and number of result
+    loadGender(gender.value,count.value)
+})
+
+function loadGender(gender, userCount){
+    let url = "https://randomuser.me/api/?results=" + userCount + "&gender=" + gender
+    console.log(url)
+
+    //call fetch, loop the result, and change the innerHTML for allGenderUser
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let resultDiv = document.getElementById("allGenderUsers")
+        let temp = ""
+        
+        for(let i = 0; i < data.results.length; i++){
+            let fn = '<div>'+ data.results[i].name.first + '</div>'
+            let ln = '<div>'+ data.results[i].name.first + '</div>'
+            let phone = '<div>'+ data.results[i].phone + '</div>'
+            let img = '<img src="' + data.results[i].picture.large + '">'
+            let gdr = '<div>' + data.results[i].gender + '</div>'
+
+            temp = temp + fn + ln + phone + gdr + img
+        }
+
+        resultDiv.innerHTML = temp
+
+    })
+}
