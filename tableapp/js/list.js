d7fcb67fa5a0089de32c8d7f198288f097b2fa87
saharaaj <raajsaha052@gmail.com>
2021-10-19 18:52:20 +0800

    added delete

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -12,6 +12,7 @@ function GetBooking() {
             console.log(json.bookings);
 
             let bookingNameList = document.getElementById("bookingNameList")
+            let bookingIds = []
 
             //delete all rows in the table
             for(let k = bookingNameList.rows.length - 1; k > 0; k--){
@@ -26,6 +27,7 @@ function GetBooking() {
                 let gPax = json.bookings[i].pax;
                 let gRemarks = json.bookings[i].remarks;
                 let gId = json.bookings[i].id;
+                let btnId = "delete" + gId;
 
                 let row = bookingNameList.insertRow(bookingNameList.rows.length)
                 row.insertCell(0).innerHTML = gId
@@ -33,7 +35,23 @@ function GetBooking() {
                 row.insertCell(2).innerHTML = gEmail
                 row.insertCell(3).innerHTML = gPax
                 row.insertCell(4).innerHTML = gRemarks
-                row.insertCell(5).innerHTML = ""
+                row.insertCell(5).innerHTML = "<button id+" + btnId + " type='button' class='btn btn-danger'>Delete</button>"
+
+                bookingIds.push(btnId)
             }
+            
+            for(let j = 0; j < bookingIds.length; j++){
+                let el = document.getElementById(bookingIds[j])
+                el.addEventListener("click", function(){
+                    let theId = el.id.replace("delete","")
+                    DeleteBooking(theId)
+                })
+            }
+
+
         });
+}
+
+function DeleteBooking(id){
+    console.log("received id = " + id)
 }
